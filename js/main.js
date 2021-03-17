88900edce862b8ec8033bb1da62624f6b2319362
shivansharora <s.arora@salubrioustech.com>
2021-03-17 13:15:31 +0530

    Simple service worker example

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 if(navigator.serviceWorker){
   window.addEventListener('load',()=>{
     navigator.serviceWorker
-    .register('../sw_pages.js')
+    .register('/sw_pages.js')
     .then(reg=>console.log('Service worker registered'))
     .catch(err=>console.log(`Service worker Erroro :${err}`))
   });
